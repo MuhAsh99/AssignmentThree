9be893b7c2f71bb970aab33e91af4890d6b00afb
MuhAsh99 <mjashraf@smu.edu>
2022-10-22 02:40:19 -0500

    GameViewController.swift
    added GameViewController file

---
+++ AssignmentThree/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  AssignmentThree
+//
+//  Created by Muhammad Ashraf on 10/22/22.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    //
+
+}
