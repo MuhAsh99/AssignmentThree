72445f8f72d0bf5d77f1f0a55a9c518cea0a5f00
MuhAsh99 <mjashraf@smu.edu>
2022-10-22 03:16:54 -0500

    Update PongGameScene.swift
    added some code for pong game

--- AssignmentThree/PongGameScene.swift
+++ AssignmentThree/PongGameScene.swift
@@ -11,5 +11,27 @@ import CoreMotion
 
 class PongGameScene: SKScene, SKPhysicsContactDelegate {
     
+    //creating paddles for pong game
+    let paddle1 = SKSpriteNode()
+    let paddle2 = SKSpriteNode()
+    
+    //label for scoring
+    let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+    
+    //creating game score var
+    var gameScore:Int = 0{
+        willSet(newValue){
+            DispatchQueue.main.async {
+                //score will update the label
+                self.scoreLabel.text = "Score: \(newValue)"
+            }
+        }
+    }
+    
+    func addBall(){
+        let ballSprite = SKSpriteNode(imageNamed: <#T##String#>)
+    }
+    
+    
 
 }
