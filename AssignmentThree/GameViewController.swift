311c5fc930854074781044e0486abd24962664be
MuhAsh99 <mjashraf@smu.edu>
2022-10-22 02:44:33 -0500

    PongGameScene.swift
    added game scene file

--- AssignmentThree/GameViewController.swift
+++ AssignmentThree/GameViewController.swift
@@ -6,25 +6,21 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //setting up the game scene
+       // let gameScene = GameScene
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    //
+    
+   
 
 }
---
+++ AssignmentThree/PongGameScene.swift
@@ -0,0 +1,14 @@
+//
+//  PongGameScene.swift
+//  AssignmentThree
+//
+//  Created by Muhammad Ashraf on 10/22/22.
+//
+
+import UIKit
+import SpriteKit
+import CoreMotion
+
+class PongGameScene: SKScene, SKPhysicsContactDelegate {
+
+}
